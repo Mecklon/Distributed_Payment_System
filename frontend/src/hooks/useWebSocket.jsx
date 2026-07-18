08bc45431a7f6629ccd4a26b91b7a84042070a70
Mecklon <fernandesmecklon3@gmail.com>
2026-07-18 22:32:55 +0530

    updated configs for deployment and local support

--- frontend/src/hooks/useWebSocket.jsx
+++ frontend/src/hooks/useWebSocket.jsx
@@ -1,16 +1,19 @@
 import { useMemo ,useEffect} from "react";
 import { Client } from "@stomp/stompjs";
 
+const brokerURL = import.meta.env.VITE_USE_NGINX === "true"
+  ? `${window.location.protocol === "https:" ? "wss" : "ws"}://${window.location.host}/ws`
+  : "ws://localhost:9092/ws";
+
 export function useWebSocket(token) {
   const client = useMemo(() => {
     return new Client({
-      brokerURL: "ws://localhost:9092/ws",
+      brokerURL,
       heartbeatIncoming: 4000,
       heartbeatOutgoing: 4000,
       connectHeaders: {
         Authorization: `Bearer ${token}`, 
       },
-   
       reconnectDelay: 5000, 
     });
   }, [token]);
